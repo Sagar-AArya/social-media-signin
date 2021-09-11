bcdd2a6410c667cf9ff5da62ae74d520f5703d69
Sagar R <sksagar.r@gmail.com>
2021-09-11 17:43:13 +0530

    Removed google credentials

--- controller/googleSignIn.js
+++ controller/googleSignIn.js
@@ -4,9 +4,9 @@ const googleSignIn = (function(){
 
     function getGoogleCreds() {
         let obj = {
-            clientId: "91259364304-7ag6jfjip527sfuggil44egvd7uqka3p.apps.googleusercontent.com",
-            clientSecret: "76FaQHWQVzJIMw08aNxtn39w",
-            apiKey: "AIzaSyDLJ-w9k6R5o8ZInze2vSA6ob4IWzZ6F4g"
+            clientId: "",
+            clientSecret: "",
+            apiKey: ""
         }
 
         return obj;
